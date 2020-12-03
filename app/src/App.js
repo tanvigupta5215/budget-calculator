020f14b0bd258adfa23f88d469341c7899dcabc9
Tanvi Gupta <tanvi.gupta5215@gmail.com>
2020-12-03 18:36:33 +0530

    Budget Calculator added

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,23 @@
+// eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
+// eslint-disable-next-line no-unused-vars
+import Button from "react-bootstrap/button";
 import './App.css';
+import React from "react";
+import MonthlyBudgetCalculator from "./monthly-saving-calculator/MonthlyBudgetCalculator";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    Budget Calculator
+                </header>
+                <MonthlyBudgetCalculator />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ app/src/monthly-saving-calculator/MonthlyBudgetCalculator.js
@@ -0,0 +1,12 @@
+import React from "react";
+class MonthlyBudgetCalculator extends React.Component {
+    render() {
+        return(
+            <div>
+                MonthlyBudget
+            </div>
+        );
+    }
+}
+
+export default MonthlyBudgetCalculator;
