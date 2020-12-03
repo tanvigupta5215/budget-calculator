599e5071e8f81ea5171420c6da9072ca3d8562f0
Tanvi Gupta <tanvi.gupta5215@gmail.com>
2020-12-03 19:19:44 +0530

    checkboxadded

--- app/src/monthly-saving-calculator/MonthlyBudgetCalculator.js
+++ app/src/monthly-saving-calculator/MonthlyBudgetCalculator.js
@@ -2,8 +2,19 @@ import React from "react";
 class MonthlyBudgetCalculator extends React.Component {
     render() {
         return(
-            <div>
-                MonthlyBudget
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                    <div class="form-check">
+                       <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+                       <label class="form-check-label" for="defaultCheck1">
+                            <p>Monthly pay=</p>
+                            <p>(take home pay)</p>
+                       </label>
+                    </div>
+                    </div> 
+                </div>
+
             </div>
         );
     }
